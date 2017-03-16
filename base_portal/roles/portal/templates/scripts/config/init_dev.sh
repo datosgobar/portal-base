e7c0b8eda3d7ae4096bd35aac4ecd56b372732e4
Leandro Gomez <lgomez@devartis.com>
2017-03-16 17:25:45 -0300

    Actualizo script de inicializacion para desarrollo
    refs:datosgobar/portal-andino#24

--- base_portal/roles/portal/templates/scripts/config/init_dev.sh
+++ base_portal/roles/portal/templates/scripts/config/init_dev.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 current_dir="$(dirname "$0")"
-CKAN_IP=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 
-"$current_dir/init.sh" -u "$CKAN_IP" -e admin@example.com -h localhost -p ckan -P ckan -d ckan_datastore -D ckan_datastore
+"$current_dir/init.sh" -e admin@example.com -h localhost -p ckan -P ckan -d ckan_datastore -D ckan_datastore
