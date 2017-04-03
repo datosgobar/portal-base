18ba9e90d17b3ca8a519d90145d29f317eb250b0
Leandro Gomez <lgomez@devartis.com>
2017-04-03 16:21:08 -0300

    Simplifico el container de nginx

--- nginx/command.sh
+++ nginx/command.sh
@@ -1,10 +1,3 @@
 set -e;
-: ${CKAN_HOST:=portal};
-NGINX_CONF=/etc/nginx/conf.d/default.conf;
-cat /etc/nginx/conf.d/andino.template > $NGINX_CONF;
-
-echo "Using host: $CKAN_HOST";
-sed -i.bak s/CKAN_HOST/$CKAN_HOST/g $NGINX_CONF;
-
 echo "Stating nginx...";
 nginx -g 'daemon off;'
