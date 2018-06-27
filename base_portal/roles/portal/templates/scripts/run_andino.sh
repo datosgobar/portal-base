4d226c1d20c004e8ae7addc24e99ad92becc1051
Christian Digiorno <cdigiorno@devartis.com>
2018-06-27 12:32:30 -0300

    Configuración de workers RQ
    Usamos supervisor para configurar dos workers para tareas asincrónicas de python-rq (una para la cola default de CKAN y otra para tareas específicas de Andino).
    Durante la instalación de Andino reiniciamos los workers, que requieren que Andino esté correctamente instalado antes de arrancar.
    Correcciones menores de textos.

--- base_portal/roles/portal/templates/scripts/run_andino.sh
+++ base_portal/roles/portal/templates/scripts/run_andino.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
 set -e
 
+service supervisor start
+
 . /etc/apache2/envvars
 exec /usr/sbin/apache2 -D FOREGROUND
-
-exec sudo apt-get update
-exec sudo apt-get -y install supervisor
-exec sudo cp /etc/ckan/default/supervisor-ckan-worker.conf /etc/supervisor/conf.d
-exec echo "Traje el archivo supervisor-ckan-worker.conf (con exec)"
-exec echo "" exec echo "" exec echo "" exec echo "" exec echo "" exec echo "" exec echo "" exec echo ""
-exec sudo service supervisor restart
-
-RUN sudo apt-get update
-RUN sudo apt-get -y install supervisor
-RUN sudo cp /etc/ckan/default/supervisor-ckan-worker.conf /etc/supervisor/conf.d
-RUN echo "Traje el archivo supervisor-ckan-worker.conf (con RUN)"
-RUN echo "" exec echo "" exec echo "" exec echo "" exec echo "" exec echo "" exec echo "" exec echo ""
-RUN sudo service supervisor restart
