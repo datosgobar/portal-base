c673699267d4fb710c282b11c9722f66768d1c66
Leandro Gomez <lgomez@devartis.com>
2018-07-17 11:49:47 -0300

    Hago configurable la opcion 'inactive' de la cache de nginx

--- nginx/command.sh
+++ nginx/command.sh
@@ -15,15 +15,15 @@ max_size="$NGINX_DEFAULT_CACHE_MAX_SIZE";
 if [[ -n "$NGINX_CACHE_MAX_SIZE" ]]; then
     max_size="$NGINX_CACHE_MAX_SIZE";
 fi
-sed -i "s/max_size=[[:alnum:]]*[[:space:]]/max_size=$max_size /" /etc/nginx/conf.d/default.conf
+sed -i "s/max_size=[[:alnum:]]*/max_size=$max_size/" /etc/nginx/conf.d/default.conf
 
 
 # Configure inactive cache option
-#inactive="$NGINX_DEFAULT_CACHE_INACTIVE";
-#if [[ -n "$NGINX_CACHE_INACTIVE" ]]; then
-#    inactive="$NGINX_CACHE_INACTIVE";
-#fi
-#sed -i "s/inactive=[^\s]*/inactive=$inactive /" /etc/nginx/conf.d/default.conf
+inactive="$NGINX_DEFAULT_CACHE_INACTIVE";
+if [[ -n "$NGINX_CACHE_INACTIVE" ]]; then
+    inactive="$NGINX_CACHE_INACTIVE";
+fi
+sed -i "s/inactive=[[:alnum:]]*/inactive=$inactive/" /etc/nginx/conf.d/default.conf
 
 
 echo "Stating nginx (with openresty)...";
