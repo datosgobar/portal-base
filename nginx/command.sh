9522f03888586c8b62ca2f7abe4fc605e6ab29f3
Leandro Gomez <lgomez@devartis.com>
2018-07-17 11:22:29 -0300

    Hago configurable el tamaño de la cache de nginx
    refs:#datosgobar/portal-andino#140

--- nginx/command.sh
+++ nginx/command.sh
@@ -10,5 +10,23 @@ else
     ln -s "$NGINX_AVAILABLE_SITES/$NGINX_DEFAULT_CONFIG_FILE" /etc/nginx/conf.d/default.conf;
 fi
 
+# Configure max_size cache option
+max_size="$NGINX_DEFAULT_CACHE_MAX_SIZE";
+if [[ -n "$NGINX_CACHE_MAX_SIZE" ]]; then
+    max_size="$NGINX_CACHE_MAX_SIZE";
+fi
+sed -i "s/max_size=[^\s]*/max_size=$max_size /" /etc/nginx/conf.d/default.conf
+
+
+#inactive="$NGINX_DEFAULT_CACHE_INACTIVE";
+#if [[ -n "$NGINX_CACHE_INACTIVE" ]]; then
+#    inactive="$NGINX_CACHE_INACTIVE";
+#fi
+#
+#
+#sed -i 's/original/new/g' /etc/nginx/conf.d/default.conf
+
+sed -i "s/proxy_cache_path.*/&;/" /etc/nginx/conf.d/default.conf
+
 echo "Stating nginx (with openresty)...";
 /usr/local/openresty/bin/openresty -g "daemon off;";
