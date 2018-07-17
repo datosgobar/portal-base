183399ed57a8fe63d114d2918911fc50f52346a6
Leandro Gomez <lgomez@devartis.com>
2018-07-17 11:38:06 -0300

    Arreglo regex de sed Solo modifica la opcion de nginx

--- nginx/command.sh
+++ nginx/command.sh
@@ -15,18 +15,16 @@ max_size="$NGINX_DEFAULT_CACHE_MAX_SIZE";
 if [[ -n "$NGINX_CACHE_MAX_SIZE" ]]; then
     max_size="$NGINX_CACHE_MAX_SIZE";
 fi
-sed -i "s/max_size=[^\s]*/max_size=$max_size /" /etc/nginx/conf.d/default.conf
+sed -i "s/max_size=[[:alnum:]]*[[:space:]]/max_size=$max_size /" /etc/nginx/conf.d/default.conf
 
 
+# Configure inactive cache option
 #inactive="$NGINX_DEFAULT_CACHE_INACTIVE";
 #if [[ -n "$NGINX_CACHE_INACTIVE" ]]; then
 #    inactive="$NGINX_CACHE_INACTIVE";
 #fi
-#
-#
-#sed -i 's/original/new/g' /etc/nginx/conf.d/default.conf
+#sed -i "s/inactive=[^\s]*/inactive=$inactive /" /etc/nginx/conf.d/default.conf
 
-sed -i "s/proxy_cache_path.*/&;/" /etc/nginx/conf.d/default.conf
 
 echo "Stating nginx (with openresty)...";
 /usr/local/openresty/bin/openresty -g "daemon off;";
