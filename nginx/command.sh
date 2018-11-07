a7095628515edb5b52b98b40f4f2711fcbcf6b68
Christian Digiorno <cdigiorno@devartis.com>
2018-11-07 11:11:59 -0300

    El archivo de configuración de nginx para SSL se guarda junto con los demás.

--- nginx/command.sh
+++ nginx/command.sh
@@ -2,6 +2,19 @@
 set -e;
 
 rm -f /etc/nginx/conf.d/default.conf;
+
+# Por el momento, no se puede utilizar este bloque ya que se requieren los archivos de configuración; usamos el original
+#if [ -d "$NGINX_AVAILABLE_SITES" ] ; then
+#    ln -s "$NGINX_SSL_CONFIG_FILE" /etc/nginx/conf.d/default.conf;
+#else
+#    if [[ -n "$NGINX_CONFIG_FILE" ]]; then
+#        echo "Using '$NGINX_CONFIG_FILE' configuration"
+#        ln -s "$NGINX_AVAILABLE_SITES/$NGINX_CONFIG_FILE" /etc/nginx/conf.d/default.conf;
+#    else
+#        echo "Using '$NGINX_DEFAULT_CONFIG_FILE' default configuration"
+#        ln -s "$NGINX_AVAILABLE_SITES/$NGINX_DEFAULT_CONFIG_FILE" /etc/nginx/conf.d/default.conf;
+#    fi
+#fi
 if [[ -n "$NGINX_CONFIG_FILE" ]]; then
     echo "Using '$NGINX_CONFIG_FILE' configuration"
     ln -s "$NGINX_AVAILABLE_SITES/$NGINX_CONFIG_FILE" /etc/nginx/conf.d/default.conf;
