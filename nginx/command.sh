3d27fa6bf8c91025883d37c9ebc74de33130d18d
Poli García <poli@devartis.com>
2018-11-20 17:39:04 -0300

    Update command.sh
    Elimino espacio en blanco

--- nginx/command.sh
+++ nginx/command.sh
@@ -2,7 +2,6 @@
 set -e;
 
 rm -f /etc/nginx/conf.d/default.conf;
-
 if [[ -n "$NGINX_CONFIG_FILE" ]]; then
     echo "Using '$NGINX_CONFIG_FILE' configuration"
     ln -s "$NGINX_AVAILABLE_SITES/$NGINX_CONFIG_FILE" /etc/nginx/conf.d/default.conf;
